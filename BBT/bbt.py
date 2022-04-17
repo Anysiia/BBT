6ece53d88fbeff71ec44e72fd8df412dcc489125
Anysiia <anysia74@hotmail.com>
2022-04-17 11:52:04 +0200

    First working print

--- BBT/bbt.py
+++ BBT/bbt.py
@@ -1,11 +1,16 @@
 from twitchio.ext import commands
+import config_bbt as cfg
 
 
-class Bot(commands.bot):
+class Bot(commands.Bot):
 
     def __init__(self):
-        super().__init__(token='ACCESS_TOKEN', prefix='?', initial_channels=['...'])
+        super().__init__(token=cfg.twitchio_config["OAUTH_TOKEN"],
+                         prefix='?',
+                         initial_channels=cfg.twitchio_config["CHANNEL_NAME"])
 
+    async def event_ready(self):
+        await self.connected_channels[0].send("Hello")
 
 bot = Bot()
 bot.run()
--- README.md
+++ README.md
@@ -4,6 +4,12 @@ A twitch bot with OBS integration
 ### Install
 `pip install -r requierements.txt`
 
+Copy `config.py.sample` into `config_bbt.py` and replace the variables with your own.
+
 
 ### Utils links
 https://twitchio.dev/en/latest/index.html
+
+How to set up the environment variables
+
+https://dev.twitch.tv/docs/irc
