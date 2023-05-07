62c2d4c03d26a4753ad7ffa595ac754ae2bd6379
misterLeu <leu.jeremy@gmail.com>
2023-05-07 11:37:01 +0200

    add minor exceptions handling, add possibility to parametrize start message, move run management to root under run.py

--- BBT/bbt.py
+++ BBT/bbt.py
@@ -1,16 +1,24 @@
 from twitchio.ext import commands
-import config_bbt as cfg
+from config_bbt import twitchio_config as config
 
 
-class Bot(commands.Bot):
+class BetterBotTwitchException(Exception):
+    """Exception specific to the bot"""
+
+
+class WrongConfigException(BetterBotTwitchException):
+    """Exception for missing configuration"""
+
+
+class BetterBotTwitch(commands.Bot):
 
     def __init__(self):
-        super().__init__(token=cfg.twitchio_config["OAUTH_TOKEN"],
+        if "OAUTH_TOKEN" not in config or "CHANNEL_NAME" not in config:
+            raise WrongConfigException
+        super().__init__(token=config["OAUTH_TOKEN"],
                          prefix='?',
-                         initial_channels=cfg.twitchio_config["CHANNEL_NAME"])
+                         initial_channels=config["CHANNEL_NAME"])
 
     async def event_ready(self):
-        await self.connected_channels[0].send("Hello")
-
-bot = Bot()
-bot.run()
+        message = config["START_MESSAGE"] if "START_MESSAGE" in config else "Hello"
+        await self.connected_channels[0].send(message)
